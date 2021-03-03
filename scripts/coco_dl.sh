e11a979b40a53d4db57c1d786f1a34a22ee33447
Robert Lee <32447910+robertklee@users.noreply.github.com>
2021-03-03 00:00:40 -0800

    update script with curl and our repo structure

--- scripts/coco_dl.sh
+++ scripts/coco_dl.sh
@@ -1,15 +1,15 @@
 # Credit to https://gist.github.com/mkocabas/a6177fc00315403d31572e17700d7fd9
 
-dataset_path = './data'
+dataset_path="./data"
 
-cd dataset_path
+cd $dataset_path
 mkdir coco
 cd coco
 
-wget -c http://images.cocodataset.org/zips/train2017.zip
-wget -c http://images.cocodataset.org/zips/val2017.zip
-wget -c http://images.cocodataset.org/zips/test2017.zip
-wget -c http://images.cocodataset.org/zips/unlabeled2017.zip
+curl -OL http://images.cocodataset.org/zips/train2017.zip
+curl -OL http://images.cocodataset.org/zips/val2017.zip
+curl -OL http://images.cocodataset.org/zips/test2017.zip
+curl -OL http://images.cocodataset.org/zips/unlabeled2017.zip
 
 unzip train2017.zip
 unzip val2017.zip
@@ -22,10 +22,10 @@ rm test2017.zip
 rm unlabeled2017.zip 
 
 cd ../
-wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
-wget http://images.cocodataset.org/annotations/stuff_annotations_trainval2017.zip
-wget http://images.cocodataset.org/annotations/image_info_test2017.zip
-wget http://images.cocodataset.org/annotations/image_info_unlabeled2017.zip
+curl -OL http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+curl -OL http://images.cocodataset.org/annotations/stuff_annotations_trainval2017.zip
+curl -OL http://images.cocodataset.org/annotations/image_info_test2017.zip
+curl -OL http://images.cocodataset.org/annotations/image_info_unlabeled2017.zip
 
 unzip annotations_trainval2017.zip
 unzip stuff_annotations_trainval2017.zip
