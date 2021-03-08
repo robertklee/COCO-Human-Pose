5376ca5ad968ecd21d66581db6ec045b6e7f9b75
Julian Rocha <jules.r.rocha@gmail.com>
2021-03-07 16:07:24 -0800

    Response to PR feedback

--- data_generator.py
+++ data_generator.py
@@ -5,17 +5,19 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import cv2
-import matplotlib.image as mpimg
 import os
 import tensorflow as tf
-import skimage.io as io
 from tensorflow.keras.utils import Sequence
+
+from constants import DEFAULT_BATCH_SIZE
+from constants import NUM_COCO_KEYPOINTS
+from constants import NUM_COCO_KP_ATTRBS
+from constants import OUTPUT_DIM
 
 class DataGenerator(Sequence):
 
-  def __init__(self, csv_file, base_dir, output_size, shuffle=False, batch_size=10):
+  def __init__(self, csv_file, base_dir, output_size, shuffle=False, batch_size=DEFAULT_BATCH_SIZE):
     self.df = pd.read_csv(csv_file) # csv containing df of the chosen annotations
     self.base_dir = base_dir        # where to read imgs from in collab runtime
     self.output_size = output_size
@@ -33,44 +35,37 @@ class DataGenerator(Sequence):
   def __len__(self):
     return int(len(self.df) / self.batch_size)
 
-  # scale RGB channels to [0,1]
-  def scale_input(self,img):
-    return img/255.0 
-
-  # resize img to input shape (will likely replace once we start dealing with bboxes)
-  def resize_img(self,img):
-    return cv2.resize(img,self.output_size,interpolation=cv2.INTER_LINEAR) # what interpolation is best?
-
-  # adjust x/y coords to new resized img
-  def resize_label(self,label,og_width,og_height):
+  def transform_image(self, img):
+    og_width, og_height = int(img.shape[1]), int(img.shape[0])
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # fix RBG so images are easier to plot
+    img = img/255.0 # scale RGB channels to [0,1]
+    img = cv2.resize(img,self.output_size,interpolation=cv2.INTER_LINEAR) # resize
+    return img, og_width, og_height
+  
+  def transform_label(self,label, og_width, og_height):
+    label = label.strip('][').split(', ')
     x_scale = self.output_size[0]/og_width
     y_scale = self.output_size[1]/og_height
-    return [int(round(int(val)*(x_scale if i%3==0 else y_scale if i%3==1 else 1))) for i,val in enumerate(label)]
+    # adjust x/y coords to new resized img
+    transformed_label = [int(round(int(val)*(x_scale if i%NUM_COCO_KP_ATTRBS==0 else y_scale if i%NUM_COCO_KP_ATTRBS==1 else 1))) for i,val in enumerate(label)]
+    return np.asarray(transformed_label)
 
   # returns batch at index idx
   def __getitem__(self, idx):
-    ## Initialize Batch:
+    # Initialize Batch:
     X = np.empty((self.batch_size, *self.output_size, 3)) # 3 channels of RGB
-    y = np.empty((self.batch_size, 3 * 17)) # (x,y,v) * 17 keypoints
+    y = np.empty((self.batch_size, NUM_COCO_KP_ATTRBS * NUM_COCO_KEYPOINTS))
 
     # get the indices of the requested batch
     indices = self.indices[idx*self.batch_size:(idx+1)*self.batch_size]
-
     for i, data_index in enumerate(indices):
-      # Load label and img
       ann = self.df.loc[data_index]
-      img = mpimg.imread(os.path.join(self.base_dir,ann['path'])) # bottleneck
+      img_path = os.path.join(self.base_dir,ann['path'])
+      img = cv2.imread(img_path) # bottleneck
 
-      # Preprocess img
-      img = DataGenerator.scale_input(self,img)
-      og_width, og_height = int(img.shape[1]), int(img.shape[0])
-      img = DataGenerator.resize_img(self,img)
-      
-      # Preprocess label
-      label = ann['keypoints'].strip('][').split(', ')
-      label = DataGenerator.resize_label(self,label,og_width,og_height)
+      transformed_img, og_width, og_height = self.transform_image(img)
+      transformed_label = self.transform_label(ann['keypoints'],og_width,og_height)
 
-      # Accumulate
-      X[i,] = img
-      y[i,] = np.asarray(label)
+      X[i,] = transformed_img
+      y[i,] = transformed_label
     return X, y
