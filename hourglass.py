934e969c8a52dd0ee44f4e8965bc187320e0ef2b
Robert Lee <32447910+robertklee@users.noreply.github.com>
2021-03-11 12:09:08 -0800

    add code for training from scratch. Working locally but needs some work for Colab

--- hourglass.py
+++ hourglass.py
@@ -3,14 +3,17 @@ import os
 
 import numpy as np
 import scipy.misc
-from keras.callbacks import CSVLogger, ModelCheckpoint
+from keras.callbacks import CSVLogger, ModelCheckpoint, TensorBoard, LearningRateScheduler
 from keras.losses import mean_squared_error
 from keras.models import load_model, model_from_json
 from keras.optimizers import Adam, RMSprop
 
 from hourglass_blocks import (bottleneck_block, bottleneck_mobile,
                               create_hourglass_network)
+from data_generator import DataGenerator
+from constants import *
 
+# Some code adapted from https://github.com/yuanyuanli85/Stacked_Hourglass_Network_Keras/blob/master/src/net/hourglass.py
 
 class HourglassNet(object):
 
@@ -33,9 +36,29 @@ class HourglassNet(object):
             self.model.summary()
         
     def train(self, batch_size, model_path, epochs):
-        # TODO
-        pass
+        train_generator = DataGenerator(DEFAULT_TRAIN_ANNOT_PATH, DEFAULT_TRAIN_IMG_PATH, self.inres, self.outres, self.num_stacks, shuffle=True, batch_size=batch_size)
+        val_generator = DataGenerator(DEFAULT_VAL_ANNOT_PATH, DEFAULT_VAL_IMG_PATH, self.inres, self.outres, self.num_stacks, shuffle=True, batch_size=batch_size)
+        
+        current_time = datetime.today().strftime('%Y-%m-%d-%Hh-%Mm')
+
+        csv_logger = CSVLogger(os.path.join(model_path, 'csv_tr' + current_time + '.csv'))
+
+        modelSavePath = os.path.join(model_path, current_time +  '_batchsize_' + str(batch_size), '/hg_epoch{epoch:02d}_val_loss_{val_loss:.4f}_train_loss_{loss:.4f}.hdf5')
+
+        mc_val = ModelCheckpoint(modelSavePath, monitor='val_loss')
+        mc_train = ModelCheckpoint(modelSavePath, monitor='loss')
+        tb = TensorBoard(log_dir=os.path.join(DEFAULT_LOGS_PATH, current_time + '_batchsize_' + str(batch_size)), histogram_freq=0, write_graph=True, write_images=True)
+
+        # TODO potentially add learning rate scheduler callback
+
+        callbacks = [mc_val, mc_train, tb, csv_logger]
+
+        print("Model saved to: {}".format(modelSavePath))
+
+        self.model.fit_generator(generator=train_generator, validation_data=val_generator, steps_per_epoch=len(train_generator), \
+            validation_steps=len(val_generator), epochs=epochs, callbacks=callbacks)
     
+    # TODO resume and load model
     def resume_train(self, batch_size, model_json, model_weights, init_epoch, epochs):
         # TODO
         pass
--- train.py
+++ train.py
@@ -1,16 +1,21 @@
 import argparse
 import os
+import time
+from datetime import datetime, timedelta
 
 import tensorflow as tf
 from keras import backend as k
 
-import constants
+from constants import *
 from hourglass import HourglassNet
 
 # TODO change to command line arguments
 num_stack = 8
 
-if __name__ == "__main__":
+def tensorflow_setup():
+    print("TensorFlow detected the following GPU(s):")
+    tf.test.gpu_device_name()
+
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
     # os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpuID)
 
@@ -19,6 +24,7 @@ if __name__ == "__main__":
 
     # Don't pre-allocate memory; allocate as-needed
     config.gpu_options.allow_growth = True
+    ## TODO may need to turn this off for the first few trials to ensure we won't run out of GPU memory mid-training
 
     # Only allow a total of half the GPU memory to be allocated
     config.gpu_options.per_process_gpu_memory_fraction = 1.0
@@ -26,7 +32,83 @@ if __name__ == "__main__":
     # Create a session with the above options specified.
     k.tensorflow_backend.set_session(tf.Session(config=config))
 
-    xnet = HourglassNet(num_classes=constants.num_classes, num_stacks=constants.num_hg_default, num_channels=256, inres=(256, 256),
+def process_args():
+    argparser = argparse.ArgumentParser(description='Training parameters')
+    argparser.add_argument('-m',
+                        '--model-save',
+                        default=DEFAULT_MODEL_PATH,
+                        help='path to save model')
+    argparser.add_argument('-e',
+                        '--epochs',
+                        default=DEFAULT_EPOCHS,
+                        type=int,
+                        help='number of epochs')
+    argparser.add_argument('-b',
+                        '--batch',
+                        type=int,
+                        default=DEFAULT_BATCH_SIZE,
+                        help='batch size')
+    argparser.add_argument('-hg',
+                        '--hourglass',
+                        type=int,
+                        default=DEFAULT_NUM_HG,
+                        help='number of hourglass blocks')
+    # Resume model training arguments
+    # TODO make a consistent way to generate and retrieve epoch checkpoint filenames
+    argparser.add_argument('-r',
+                        '--resume',
+                        default=False,
+                        type=bool,
+                        help='resume training')
+    argparser.add_argument('--resume-json',
+                        default=None,
+                        help='Model architecture for re-loading weights to resume training')
+    argparser.add_argument('--resume-weights',
+                        default=None,
+                        help='Model weights file to resume training')
+    argparser.add_argument('--resume-epoch',
+                        default=None,
+                        type=int,
+                        help='Epoch to resume training')
+
+    # Convert string arguments to appropriate type
+    args = argparser.parse_args()
+
+    return args
+
+if __name__ == "__main__":
+    args = process_args()
+
+    print("\n\nSetup start: {}\n".format(time.ctime()))
+    setup_start = time.time()
+
+    tensorflow_setup()
+
+    trainingRunTime = datetime.today().strftime('%Y-%m-%d-%Hh-%Mm')
+
+    hgnet = HourglassNet(num_classes=NUM_COCO_KEYPOINTS, num_stacks=args.hourglass, num_channels=256, inres=(256, 256),
                             outres=(64, 64))
-    
-    xnet.build_model(show=True)
+
+    training_start = time.time()
+
+    if args.resume:
+        print("\n\Resume training start: {}\n".format(time.ctime()))
+
+        hgnet.resume_train(args.batch, args.resume_json, args.resume_weights, args.resume_epoch, args.epochs)
+    else:
+        hgnet.build_model(show=True)
+
+        print("\n\nTraining start: {}\n".format(time.ctime()))
+        print("Hourglass blocks: {:2d}, epochs: {:3d}, batch size: {:2d}".format(args.hourglass, args.epochs, args.batch))
+
+        hgnet.train(args.batch, args.model_save, args.epochs)
+
+    print("\n\nTraining end:   {}\n".format(time.ctime()))
+
+    training_end = time.time()
+
+    setup_time = training_start - setup_start
+    training_time = training_end - training_start
+
+    print("Total setup time: {}".format(str(timedelta(seconds=setup_time))))
+    print("Total train time: {}".format(str(timedelta(seconds=training_time))))
