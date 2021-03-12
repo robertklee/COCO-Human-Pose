d325ad62bb585a0121afd8654ecf4c5fed4eb4a6
Robert Lee <32447910+robertklee@users.noreply.github.com>
2021-03-12 11:03:54 -0800

    try to add a separate case if COCO tools API is not installed. BROKEN

--- constants.py
+++ constants.py
@@ -1,6 +1,9 @@
 # Colab Training
 COLAB_TRAINING = False
 
+# Coco tools installed
+COCO_TOOLS_INSTALLED = False
+
 # Model Defaults
 DEFAULT_NUM_HG = 4 # NOTE in their final design, 8 were used
 DEFAULT_EPOCHS = 40
