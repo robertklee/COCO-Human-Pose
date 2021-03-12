2bda3b6f30eae6805e8c2c78f64b1f72bc2f753f
Julian Rocha <jules.r.rocha@gmail.com>
2021-03-12 12:12:12 -0800

    Added crowd and bbox size filters, added timer test to data generator, updated coco-explorer to use coco-df module

--- hourglass.py
+++ hourglass.py
@@ -39,6 +39,10 @@ class HourglassNet(object):
     def load_and_filter_annotations(self,path_to_train_anns,path_to_val_anns):
         df = coco_df.get_df(path_to_train_anns,path_to_val_anns)
         # apply filters here
+        print(f"Unfiltered df contains {len(df)} anns")
+        df = df.loc[df['is_crowd'] == 0] # filter crowd
+        df = df.loc[df['bbox_area'] > 900] # filter small bboxes (30*30)
+        print(f"Filtered df contains {len(df)} anns")
         train_df = df.loc[df['source'] == 0]
         val_df = df.loc[df['source'] == 1]
         return train_df.reset_index(), val_df.reset_index()
