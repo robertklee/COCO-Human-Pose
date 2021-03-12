1aaa742f513e86b03a28c5236be73d1dd3243d68
Robert Lee <32447910+robertklee@users.noreply.github.com>
2021-03-12 14:34:33 -0800

    # This is a combination of 3 commits. # This is the 1st commit message:
    update shell script to download files
    
    # This is the commit message #2:
    
    suppress verbose printing of unzip command
    
    # This is the commit message #3:
    
    add some more print statements to keep user updated

--- scripts/coco_dl.sh
+++ scripts/coco_dl.sh
@@ -1,38 +1,40 @@
 # Credit to https://gist.github.com/mkocabas/a6177fc00315403d31572e17700d7fd9
 
-dataset_path="./data"
+dataset_root_path="./data"
 
-cd $dataset_path
+# BEWARE: Symbolic links to a directory will also pass this check
+# https://stackoverflow.com/questions/59838/how-can-i-check-if-a-directory-exists-in-a-bash-shell-script
+if [ ! -d "$dataset_root_path" ]; then
+    mkdir $dataset_root_path
+fi
+
+cd $dataset_root_path
+
+#######################################
+#     Download COCO2017 Images        #
+#######################################
 mkdir coco
 cd coco
 
-curl -OL http://images.cocodataset.org/zips/train2017.zip
-curl -OL http://images.cocodataset.org/zips/val2017.zip
-curl -OL http://images.cocodataset.org/zips/test2017.zip
-curl -OL http://images.cocodataset.org/zips/unlabeled2017.zip
+for imgs_zip in "train2017" "val2017" # "test2017" "unlabeled2017"
+do
+    echo "Downloading: http://images.cocodataset.org/zips/$imgs_zip.zip"
+    curl -OL http://images.cocodataset.org/zips/$imgs_zip.zip
+    echo "Unzipping: $imgs_zip.zip"
+    unzip -q $imgs_zip.zip
+    rm $imgs_zip.zip
+done
 
-unzip train2017.zip
-unzip val2017.zip
-unzip test2017.zip
-unzip unlabeled2017.zip
-
-rm train2017.zip
-rm val2017.zip
-rm test2017.zip
-rm unlabeled2017.zip 
-
+#######################################
+#    Download COCO2017 Annotations    #
+#######################################
 cd ../
-curl -OL http://images.cocodataset.org/annotations/annotations_trainval2017.zip
-curl -OL http://images.cocodataset.org/annotations/stuff_annotations_trainval2017.zip
-curl -OL http://images.cocodataset.org/annotations/image_info_test2017.zip
-curl -OL http://images.cocodataset.org/annotations/image_info_unlabeled2017.zip
 
-unzip annotations_trainval2017.zip
-unzip stuff_annotations_trainval2017.zip
-unzip image_info_test2017.zip
-unzip image_info_unlabeled2017.zip
-
-rm annotations_trainval2017.zip
-rm stuff_annotations_trainval2017.zip
-rm image_info_test2017.zip
-rm image_info_unlabeled2017.zip
+for annos_zip in "annotations_trainval2017" "stuff_annotations_trainval2017" # "image_info_test2017" "image_info_unlabeled2017" 
+do
+    echo "Downloading: http://images.cocodataset.org/zips/$annos_zip.zip"
+    curl -OL http://images.cocodataset.org/annotations/$annos_zip.zip
+    echo "Unzipping: $annos_zip.zip"
+    unzip -q $annos_zip.zip
+    rm $annos_zip.zip
+done
