1edd569bc468dfd8d0b182dc35c2ae2b96cf43e6
Robert Lee <32447910+robertklee@users.noreply.github.com>
2021-03-12 02:24:58 -0800

    make model path dynamic based on Colab or local train

--- constants.py
+++ constants.py
@@ -1,6 +1,5 @@
-# Number of joints to detect
-num_classes = 16 #TODO change to 17 after verifying model
-num_hg_default = 4 # NOTE in their final design, 8 were used
+# Colab Training
+COLAB_TRAINING = False
 
 # Model Defaults
 DEFAULT_NUM_HG = 4 # NOTE in their final design, 8 were used
@@ -8,8 +7,21 @@ DEFAULT_EPOCHS = 40
 DEFAULT_MODEL_PATH = 'models'
 DEFAULT_LOGS_PATH = 'logs'
 DEFAULT_OUTPUT_PATH = 'output'
-DEFAULT_TRAIN_IMG_PATH = '/content/datasets/coco'
-DEFAULT_VAL_IMG_PATH = '/content/datasets/coco'
+
+# Dataset Constants
+if COLAB_TRAINING:
+    DEFAULT_TRAIN_ANNOT_PATH = '/content/gdrive/My Drive/COCO/2017/annotations/person_keypoints_train2017.json'
+    DEFAULT_VAL_ANNOT_PATH = '/content/gdrive/My Drive/COCO/2017/annotations/person_keypoints_val2017.json'
+    DEFAULT_TRAIN_IMG_PATH = '/content/datasets/coco'
+    DEFAULT_VAL_IMG_PATH = '/content/datasets/coco'
+else:
+    DEFAULT_TRAIN_ANNOT_PATH = 'data/annotations/person_keypoints_train2017.json'
+    DEFAULT_VAL_ANNOT_PATH = 'data/annotations/person_keypoints_val2017.json'
+    DEFAULT_TRAIN_IMG_PATH = 'data/coco'
+    DEFAULT_VAL_IMG_PATH = 'data/coco'
+
+COCO_TRAIN_ANNOT_PATH = DEFAULT_TRAIN_ANNOT_PATH
+COCO_VAL_ANNOT_PATH = DEFAULT_VAL_ANNOT_PATH
 
 # Model parameters
 INPUT_DIM = (256,256)
@@ -19,12 +31,3 @@ OUTPUT_DIM = (64,64)
 DEFAULT_BATCH_SIZE = 10 #NOTE need to test optimal batch size
 NUM_COCO_KEYPOINTS = 17 # Number of joints to detect
 NUM_COCO_KP_ATTRBS = 3 # (x,y,v) * 17 keypoints
-
-# COCO DF Constants
-COCO_TRAIN_ANNOT_PATH = '/content/gdrive/My Drive/COCO/2017/annotations/person_keypoints_train2017.json'
-COCO_VAL_ANNOT_PATH = '/content/gdrive/My Drive/COCO/2017/annotations/person_keypoints_val2017.json'
-
-DEFAULT_TRAIN_ANNOT_PATH = COCO_TRAIN_ANNOT_PATH
-DEFAULT_VAL_ANNOT_PATH = COCO_VAL_ANNOT_PATH
-
-# COCO 
