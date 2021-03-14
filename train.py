84b8c6f4849acb17eb5ee76bab5d54e4e0d26136
Robert Lee <32447910+robertklee@users.noreply.github.com>
2021-03-14 03:34:52 -0700

    file to automatically find the model json and hdf5 based on enclosing dir

--- train.py
+++ train.py
@@ -70,14 +70,40 @@ def process_args():
                         default=None,
                         type=int,
                         help='Epoch to resume training')
+    argparser.add_argument('--resume-subdir',
+                        default=None,
+                        help='Subdirectory containing architecture json and weights')
 
     # Convert string arguments to appropriate type
     args = argparser.parse_args()
 
     return args
 
+def find_resume_json_weights(args):
+    enclosing_dir = os.path.join(args.model_save, args.resume_subdir)
+
+    files = os.listdir(enclosing_dir)
+
+    model_jsons         = [f for f in files if (".json" in f and "hpe_hourglass_stacks" in f)]
+    model_saved_weights = [f for f in files if (".hdf5" in f and "hpe_epoch{epoch:02d}".format(args.resume_epoch) in f)]
+    
+    assert len(model_jsons) > 0
+    assert len(model_saved_weights) > 0
+
+    args.resume_json = os.path.join(enclosing_dir, model_jsons[0])
+    args.resume_weights = os.path.join(enclosing_dir, model_saved_weights[0])
+
+    print('Found model json: {}'.format(args.resume_json))
+    print('Found model weights for epoch {epoch:02d}: {weight_file_name}'.format(epoch=args.resume_epoch, weight_file_name=args.resume_weights))
+
+    assert os.path.exists(args.resume_json)
+    assert os.path.exists(args.resume_weights)
+
 if __name__ == "__main__":
     args = process_args()
+
+    if args.resume and args.resume_subdir is not None:
+        find_resume_json_weights(args)
 
     print("\n\nSetup start: {}\n".format(time.ctime()))
     setup_start = time.time()
