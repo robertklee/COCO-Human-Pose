4ead1dfa41144ad80876e29265c95d828ea0aedb
Robert Lee <32447910+robertklee@users.noreply.github.com>
2021-04-01 13:18:22 -0700

    fix pickle path

--- constants.py
+++ constants.py
@@ -60,7 +60,7 @@ else:
     DEFAULT_VAL_ANNOT_PATH = 'data/annotations/person_keypoints_val2017.json'
     DEFAULT_TRAIN_IMG_PATH = 'data/coco'
     DEFAULT_VAL_IMG_PATH = 'data/coco'
-    DEFAULT_PICKLE_PATH = './Pickles/'
+    DEFAULT_PICKLE_PATH = 'Pickles'
 
 COCO_KEYPOINT_LABEL_ARR = ["nose","left_eye","right_eye","left_ear","right_ear","left_shoulder","right_shoulder","left_elbow","right_elbow","left_wrist","right_wrist","left_hip","right_hip","left_knee","right_knee","left_ankle","right_ankle"]
 
