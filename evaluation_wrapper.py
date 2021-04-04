b47f5e196649ae94383bda12bfd6909a83d15cad
Julian Rocha <jules.r.rocha@gmail.com>
2021-04-03 19:43:15 -0700

    Added template for evaluation wrapper and model tester notebook

---
+++ evaluation_wrapper.py
@@ -0,0 +1,24 @@
+import evaluation
+
+class EvaluationWrapper():
+
+    def __init__(self):
+        print("Initialized")
+
+    def visualizeHeatmaps(self, images='representative_set.pkl'):
+        pass
+
+    def visalizeKeypoints(self, images='representative_set.pkl'):
+        pass
+
+    def calculateOKS(self, epochs):
+        pass
+
+    def calculatePCK(self, epochs):
+        pass
+
+    def plotOKS(self, epochs):
+        pass
+
+    def plotPCK(self, epochs):
+        pass
