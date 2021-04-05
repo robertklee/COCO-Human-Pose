3f2c65c4e9c8cabbe685f96afa4cfd50e3f31eb9
Robert Lee <32447910+robertklee@users.noreply.github.com>
2021-04-05 14:16:06 -0700

    add function to visualize heatmaps and/or keypoints

--- evaluation_wrapper.py
+++ evaluation_wrapper.py
@@ -46,11 +46,28 @@ class EvaluationWrapper():
         self.cocoGt = COCO(DEFAULT_VAL_ANNOT_PATH)
         print("Initialized Evaluation Wrapper!")
 
-    def visualizeHeatmaps(self, images=Generator.representative_set_gen):
-        pass
+    def visualizeHeatmaps(self, genEnum=Generator.representative_set_gen):
+        self.visualize(genEnum=genEnum, visualize_heatmaps=True, visualize_scatter=False, visualize_skeleton=False)
 
-    def visalizeKeypoints(self, images=Generator.representative_set_gen):
-        pass
+    def visualizeKeypoints(self, genEnum=Generator.representative_set_gen, visualize_skeleton=True):
+        self.visualize(genEnum=genEnum, visualize_heatmaps=False, visualize_scatter=True, visualize_skeleton=visualize_skeleton)
+
+    # Heatmaps is by default False because it is extremely processor intensive to calculate
+    def visualize(self, genEnum=Generator.representative_set_gen, visualize_heatmaps=False, visualize_scatter=True, visualize_skeleton=True):
+        gen = self._get_generator(genEnum)
+
+        for X_batch, y_stacked, m_batch in gen:
+            y_batch = y_stacked[0] # take first hourglass section
+
+            img_id_batch = [m['ann_id'] for m in m_batch] # image IDs are the annotation IDs
+
+            predicted_heatmaps_batch = self.eval.predict_heatmaps(X_batch)
+
+            if visualize_heatmaps:
+                self.eval.visualize_heatmaps(X_batch, y_batch, img_id_batch, predicted_heatmaps_batch)
+
+            if visualize_scatter or visualize_skeleton:
+                keypoints = self.eval._heatmaps_to_keypoints(predicted_heatmaps_batch[-1, 0, :, :, :])
 
     def calculateOKS(self, epochs, genEnum):
         gen = self._get_generator(genEnum)
